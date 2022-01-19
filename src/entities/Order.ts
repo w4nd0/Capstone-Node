fabc8d9cd79cc1dca719211e9bece8f02a25bb90
Wander Moreira <wandersantosmoreira@gmail.com>
2022-01-19 11:27:55 -0300

    feat: quantity column added

--- src/entities/Order.ts
+++ src/entities/Order.ts
@@ -1,15 +1,23 @@
+import { Expose } from "class-transformer";
 import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
 } from "typeorm";
+import OrderProduct from "./OrderProduct";
+import User from "./User";
 
 @Entity("orders")
 class Order {
   @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @ManyToOne(() => User)
+  user: User;
 
   @Column()
   userId: string;
@@ -25,6 +33,21 @@ class Order {
 
   @Column()
   status: string;
+
+  @OneToMany(() => OrderProduct, (orderProduct) => orderProduct.order, {
+    eager: true,
+  })
+  products: OrderProduct[];
+
+  @Expose({ name: "subtotal" })
+  getSubtotal(): number {
+    const subtotal = this.products.reduce(
+      (acc, actual) => acc + Number(actual.price * actual.quantity),
+      0
+    );
+
+    return subtotal;
+  }
 
   @CreateDateColumn()
   created_at: Date;
