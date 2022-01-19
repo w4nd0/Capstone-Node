79f3f14ed4c45c2e4b5931e93dcc1269205e2821
Wander Moreira <wandersantosmoreira@gmail.com>
2022-01-19 10:25:10 -0300

    fix: migration address

--- src/database/migrations/1642522815570-CreateAddressesTable.ts
+++ src/database/migrations/1642522815570-CreateAddressesTable.ts
@@ -2,34 +2,36 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateAddressesTable1642522815570 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    new Table({
-      name: "addresses",
-      columns: [
-        {
-          name: "id",
-          type: "uuid",
-          isPrimary: true,
-          generationStrategy: "uuid",
-          default: "uuid_generate_v4()",
-        },
-        {
-          name: "userId",
-          type: "uuid",
-        },
-        {
-          name: "city",
-          type: "varchar",
-        },
-        {
-          name: "street",
-          type: "varchar",
-        },
-        {
-          name: "number",
-          type: "int",
-        },
-      ],
-    });
+    await queryRunner.createTable(
+      new Table({
+        name: "addresses",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "userId",
+            type: "uuid",
+          },
+          {
+            name: "city",
+            type: "varchar",
+          },
+          {
+            name: "street",
+            type: "varchar",
+          },
+          {
+            name: "number",
+            type: "int",
+          },
+        ],
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
