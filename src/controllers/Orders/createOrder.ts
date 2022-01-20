a7aa424a858b7b6f6e38b8b6005eea393a405752
guilemes-freitas <guilemes300@gmail.com>
2022-01-20 11:47:01 -0300

    feature: order service e controller

---
+++ src/controllers/Orders/createOrder.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import CreateOrderService from "../../services/Orders/createOrder.service";
+
+export const create = async (request: Request, response: Response) => {
+  const createOrderService = new CreateOrderService();
+
+  const newOrder = await createOrderService.execute(request.body);
+
+  return response.json(newOrder);
+};
---
+++ src/services/Orders/createOrder.service.ts
@@ -0,0 +1,54 @@
+import { getRepository } from "typeorm";
+import AppError from "../../errors/AppError";
+
+import Order from "../../entities/Order";
+import OrderProduct from "../../entities/OrderProduct";
+import Product from "../../entities/Product";
+
+interface IOrder {
+  id: string;
+  userId: string;
+  city: string;
+  street: string;
+  number: number;
+  products_ids: string[];
+}
+
+interface IRequest {
+  order: IOrder;
+  products_ids: string[];
+}
+class CreateOrderService {
+  async execute(request: IRequest): Promise<Order | Error> {
+    try {
+      const { order, products_ids } = request;
+      const orderRepository = getRepository(Order);
+      const orderProductsRepository = getRepository(OrderProduct);
+
+      const newOrder = orderRepository.create({
+        ...order,
+      });
+
+      await orderRepository.save(order);
+
+      products_ids.forEach(async (productId) => {
+        const orderProduct = orderProductsRepository.create({
+          order: {
+            id: order.id,
+          },
+          product: {
+            id: productId,
+          },
+        });
+
+        await orderProductsRepository.save(orderProduct);
+      });
+
+      return newOrder;
+    } catch (error: any) {
+      throw new AppError(error.message);
+    }
+  }
+}
+
+export default CreateOrderService;
