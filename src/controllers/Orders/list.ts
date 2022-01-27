0eb767e02634523638f1a01ba4722eb52b9010dc
Wander Moreira <wandersantosmoreira@gmail.com>
2022-01-27 18:59:01 -0300

    controllers renamed

--- src/controllers/Orders/list.ts
+++ src/controllers/Orders/list.ts
@@ -5,8 +5,11 @@ import ListOrdersService from "../../services/Orders/ListOrder.service";
 export const list = async (request: Request, response: Response) => {
   const listOrdersService = new ListOrdersService();
 
+  const { idUser } = request.query;
+
   const listOrders = await listOrdersService.execute({
     userId: request.user.id,
+    idUser,
   });
 
   return response.json(instanceToInstance(listOrders));
--- src/services/Orders/ListOrder.service.ts
+++ src/services/Orders/ListOrder.service.ts
@@ -4,25 +4,29 @@ import User from "../../entities/User";
 
 interface Request {
   userId: string;
+  idUser: any;
 }
 
 class ListOrdersService {
-  async execute({ userId }: Request): Promise<Order[] | Error> {
+  async execute({ userId, idUser }: Request): Promise<Order[] | Error> {
     const userRepository = getRepository(User);
     const user = await userRepository.findOne({ id: userId });
 
     const ordersRepository = getRepository(Order);
+    
     let orders: Promise<Order[]>;
-
-    if (user && user.isAdm) {
-      orders = ordersRepository.find();  
+   
+    if (idUser && user?.isAdm) {
+      orders = ordersRepository.find({ where: { userId: idUser } });
+    } else if (user && user.isAdm) {
+      orders = ordersRepository.find();
     } else {
       orders = ordersRepository.find({
         where: {
           userId,
-        }
-      }); 
-    };
+        },
+      });
+    }
 
     return orders;
   }
